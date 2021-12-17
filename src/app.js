508031c0c30cc802bfaff98dcd09070e2e434444
SYO1 <zhangyiming0929@gmail.com>
2021-12-17 15:05:22 +0900

    test

--- src/app.js
+++ src/app.js
@@ -41,34 +41,34 @@
 		
 		if(p1Click === true)
 		{
-			//client.say(channel, `@${tags.username}, cool`);
-			client.say(channel, `@${tags.username}, Help`);
+			client.say(channel, `@${tags.username}, cool`);
+			//client.say(channel, `@${tags.username}, Help`);
 		}
 		else if (p2Click === true)
 		{
-			let rand = Math.floor(Math.random()*3);
-			switch(rand)
-			{
-				case 0:
-					client.say(channel, `@${tags.username}, Hello everyone!`);
-					break;
-				case 1:
-					client.say(channel, `@${tags.username}, Nice to meet you!`);
-					break;
-				case 2:
-					client.say(channel, `@${tags.username}, Let's play the game!`);
-					break;
-				default:
-					client.say(channel, `@${tags.username}, Hello.`);
-					break;
-			}
+			//let rand = Math.floor(Math.random()*3);
+			//switch(rand)
+			//{
+			//	case 0:
+			//		client.say(channel, `@${tags.username}, Hello everyone!`);
+			//		break;
+			//	case 1:
+			//		client.say(channel, `@${tags.username}, Nice to meet you!`);
+			//		break;
+			//	case 2:
+			//		client.say(channel, `@${tags.username}, Let's play the game!`);
+			//		break;
+			//	default:
+			//		client.say(channel, `@${tags.username}, Hello.`);
+			//		break;
+			//}
 			//client.say(channel, `@${tags.username}, Confuse`);
-			//client.say(channel, `@${tags.username}, good`);
+			client.say(channel, `@${tags.username}, good`);
 		}
 		else if (p3Click === true)
 		{
-			client.say(channel, `@${tags.username}, Tease`);
-			//client.say(channel, `@${tags.username}, ok`);
+			//client.say(channel, `@${tags.username}, Tease`);
+			client.say(channel, `@${tags.username}, ok`);
 		}
 		else if (p4Click === true)
 		{
@@ -119,8 +119,8 @@
             alert("Please type your full name");
         }
         else{
-			//client.say(`#syo117`, `@${name}, cool`);
-            client.say(`#syo117`, `@${name}, Help`);
+			client.say(`#syo117`, `@${name}, cool`);
+            //client.say(`#syo117`, `@${name}, Help`);
         }
         
     }
@@ -133,24 +133,24 @@
 
 		}
 		else{
-			let rand = Math.floor(Math.random()*3);
-			switch(rand)
-			{
-				case 0:
-					client.say(`#syo117`, `@${name}, Hello everyone!`);
-					break;
-				case 1:
-					client.say(`#syo117`, `@${name}, Nice to meet you!`);
-					break;
-				case 2:
-					client.say(`#syo117`, `@${name}, Let's play the game!`);
-					break;
-				default:
-					client.say(`#syo117`, `@${name}, Hello.`);
-					break;
-			}
+			//let rand = Math.floor(Math.random()*3);
+			//switch(rand)
+			//{
+			//	case 0:
+			//		client.say(`#syo117`, `@${name}, Hello everyone!`);
+			//		break;
+			//	case 1:
+			//		client.say(`#syo117`, `@${name}, Nice to meet you!`);
+			//		break;
+			//	case 2:
+			//		client.say(`#syo117`, `@${name}, Let's play the game!`);
+			//		break;
+			//	default:
+			//		client.say(`#syo117`, `@${name}, Hello.`);
+			//		break;
+			//}
 			//client.say(`#syo117`, `@${name}, Confuse`);
-			//client.say(`#syo117`, `@${name}, good`);
+			client.say(`#syo117`, `@${name}, good`);
 		}
 	}
 	function sentMessageBotP3(){
@@ -162,8 +162,8 @@
 	
 		}
 		else{
-			client.say(`#syo117`, `@${name}, Tease`);
-			//client.say(`#syo117`, `@${name}, ok`);
+			//client.say(`#syo117`, `@${name}, Tease`);
+			client.say(`#syo117`, `@${name}, ok`);
 		}
     }
 
